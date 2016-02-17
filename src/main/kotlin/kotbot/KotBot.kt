bb2fb80f39f24a0c7e017f80e6cd19e1fd3fbe0d
austinv11 <austinv11@gmail.com>
2016-02-17 17:26:56 -0500

    Enables credentials to be provided via the config.json or through by command line

--- src/main/kotlin/kotbot/KotBot.kt
+++ src/main/kotlin/kotbot/KotBot.kt
@@ -12,17 +12,20 @@ import java.io.File
  * A wrapper for Discord4J which allows for continuous uptime even through reloads.
  */
 public fun main(args: Array<String>) {
-    if (args.size < 2)
-        throw IllegalArgumentException("Expected at least two arguments (email, password)")
-    
     if (KotBot.CONFIG_FILE.exists()) {
         val loadedConfig = KotBot.GSON.fromJsonWithComments<Config>(KotBot.CONFIG_FILE)
         if (loadedConfig != null)
             KotBot.CONFIG = loadedConfig
     }
     
+    var email: String? = KotBot.CONFIG.email
+    var password: String? = KotBot.CONFIG.password
+    
+    if ((email == null || password == null) && args.size < 2)
+        throw IllegalArgumentException("Expected at least two arguments (email, password)")
+    
     try {
-        KotBot.WRAPPER = Discord4JWrapper(args[0], args[1])
+        KotBot.WRAPPER = Discord4JWrapper(email?: args[0], password?: args[1])
     } catch(e: RuntimeException) {
         KotBot.LOGGER.error("Unable to start wrapper! Aborting launch...", e)
         return
--- src/main/kotlin/kotbot/configuration/Config.kt
+++ src/main/kotlin/kotbot/configuration/Config.kt
@@ -2,6 +2,9 @@ package kotbot.configuration
 
 class Config {
     
-    @Comment("DSD")
-    var thing = false
+    @Comment("This is the email for the discord account to use. NOTE: if email or password are left null, KotBot will take account credentials through command parameters")
+    var email: String? = null
+    
+    @Comment("This is the password for the discord account to use. NOTE: if email or password are left null, KotBot will take account credentials through command parameters")
+    var password: String? = null
 }
